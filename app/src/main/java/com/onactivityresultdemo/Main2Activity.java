b8d518045766ba4bdb7341dc4f21d5ec18035810
prashant-webonise <prashant.singh@weboniselab.com>
2018-09-14 18:42:33 +0530

    Sample implementation

--- app/src/main/java/com/onactivityresultdemo/Main2Activity.java
+++ app/src/main/java/com/onactivityresultdemo/Main2Activity.java
@@ -4,38 +4,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
-
-import com.prashant.onactivityresult.annotation.OnActivityResult;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+    public static final int RESULT_CODE1 = 100;
+    public static final int RESULT_CODE2 = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+        final Button button1 = findViewById(R.id.button1);
+        final Button button2 = findViewById(R.id.button2);
+        final Button button3 = findViewById(R.id.button3);
+
+        button1.setText("resultCode = " + RESULT_CODE1);
+        button2.setText("resultCode = " + RESULT_CODE2);
+        button3.setText("resultCode (NOT HANDLED)");
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setResult(RESULT_CANCELED, new Intent());
+                final Intent intent = new Intent();
+                intent.putExtra("message", "Message from the future, resultCode = " + RESULT_CODE1);
+                setResult(RESULT_CODE1, intent);
                 finish();
             }
         });
-    }
-
-    @OnActivityResult(requestCode = 2, resultCode = RESULT_OK)
-    void action1(Intent data) {
-        Toast.makeText(this, "" + data, Toast.LENGTH_SHORT).show();
-    }
-
-    @OnActivityResult(requestCode = 3, resultCode = RESULT_OK)
-    void action2(Intent data) {
-        Toast.makeText(this, "" + data, Toast.LENGTH_SHORT).show();
-    }
-
-    @OnActivityResult(requestCode = 4, resultCode = RESULT_OK)
-    void action3(Intent data) {
-        Toast.makeText(this, "" + data, Toast.LENGTH_SHORT).show();
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent();
+                intent.putExtra("message", "Message from the future, resultCode = " + RESULT_CODE2);
+                setResult(RESULT_CODE2, intent);
+                finish();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/onactivityresultdemo/MainActivity.java
+++ app/src/main/java/com/onactivityresultdemo/MainActivity.java
@@ -5,24 +5,41 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.prashant.onactivityresult.ActivityResults;
 import com.prashant.onactivityresult.annotation.OnActivityResult;
 
+import static com.onactivityresultdemo.Main2Activity.RESULT_CODE1;
+import static com.onactivityresultdemo.Main2Activity.RESULT_CODE2;
+
 public class MainActivity extends AppCompatActivity {
 
-    public static final int REQUEST_CODE = 12;
+    public static final int REQUEST_CODE1 = 1;
+    public static final int REQUEST_CODE2 = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        final Button button1 = findViewById(R.id.button1);
+        final Button button2 = findViewById(R.id.button2);
+
+        button1.setText("requestCode = " + REQUEST_CODE1);
+        button2.setText("requestCode = " + REQUEST_CODE2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivityForResult(new Intent(MainActivity.this, Main2Activity.class), REQUEST_CODE);
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, Main2Activity.class), REQUEST_CODE1);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, Main2Activity.class), REQUEST_CODE2);
             }
         });
     }
@@ -33,18 +50,39 @@ public class MainActivity extends AppCompatActivity {
         ActivityResults.hook(this, requestCode, resultCode, data);
     }
 
-    @OnActivityResult(resultCode = RESULT_OK, requestCode = REQUEST_CODE)
-    void action1(Intent data) {
-        Toast.makeText(this, "action1 called with " + data, Toast.LENGTH_SHORT).show();
+    @OnActivityResult(requestCode = REQUEST_CODE1, resultCode = RESULT_CODE1)
+    void method1(Intent data) {
+        String msg = "requestCode = " + REQUEST_CODE1 + " , resultCode = " + RESULT_CODE1;
+        if (data != null) {
+            msg += "\n" + data.getStringExtra("message");
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 
-    @OnActivityResult(resultCode = RESULT_CANCELED, requestCode = REQUEST_CODE)
-    void action2(Intent data) {
-        Toast.makeText(this, "action2 called with " + data, Toast.LENGTH_SHORT).show();
+    @OnActivityResult(requestCode = REQUEST_CODE1, resultCode = RESULT_CODE2)
+    void method2(Intent data) {
+        String msg = "requestCode = " + REQUEST_CODE1 + " , resultCode = " + RESULT_CODE2;
+        if (data != null) {
+            msg += "\n" + data.getStringExtra("message");
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 
-    @OnActivityResult(resultCode = RESULT_OK, requestCode = REQUEST_CODE)
-    void action3(Intent data) {
-        Toast.makeText(this, "action3 called with " + data, Toast.LENGTH_SHORT).show();
+    @OnActivityResult(requestCode = REQUEST_CODE2, resultCode = RESULT_CODE1)
+    void method3(Intent data) {
+        String msg = "requestCode = " + REQUEST_CODE2 + " , resultCode = " + RESULT_CODE1;
+        if (data != null) {
+            msg += "\n" + data.getStringExtra("message");
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    @OnActivityResult(requestCode = REQUEST_CODE2, resultCode = RESULT_CODE2)
+    void method4(Intent data) {
+        String msg = "requestCode = " + REQUEST_CODE2 + " , resultCode = " + RESULT_CODE2;
+        if (data != null) {
+            msg += "\n" + data.getStringExtra("message");
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 }
